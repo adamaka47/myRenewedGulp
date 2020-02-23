6a1fdd7258b43191bd5ecd2d395aed956730d263
Adam2 <43184258+adamaka47@users.noreply.github.com>
2020-02-23 22:57:26 +0300

    Просто пушечная сборка

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,133 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let csso = require('gulp-csso')
-let htmlmin = require('gulp-htmlmin');
-let bs = require('browser-sync').create();
-let prefixer = require('gulp-autoprefixer');
-let uglify = require('gulp-uglify');
-let pipeline = require('readable-stream').pipeline;
-let babel = require('gulp-babel');
-let del = require('del');
-let imagemin = require('gulp-imagemin');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const prefixer = require('gulp-autoprefixer')
+const bs = require('browser-sync')
+const watch = require('gulp-watch')
+const babel = require('gulp-babel')
+const del = require('del')
+const sprite = require('gulp-svg-sprite')
+const sourcemaps = require('gulp-sourcemaps')
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const imagemin = require('gulp-imagemin')
+const fileinc = require('gulp-file-include')
+const concat = require('gulp-concat')
+const cheerio = require('gulp-cheerio')
+const htmlmin = require('gulp-htmlmin')
+const uglify = require('gulp-uglify')
+const replace = require('gulp-replace')
 
+gulp.task('server', function() {
 
-function streamMe() {
 	bs.init({
 		server: {
-			baseDir: './src'
+			baseDir: './build'
 		}
-		})
+	})
 
-	gulp.watch('src/index.html', gulp.series(minifyHtml)).on('change', bs.reload)
-	gulp.watch('src/*.js', gulp.series(compressJS)).on('change', bs.reload)
-	gulp.watch('src/sass/main.sass', gulp.series(sassCompile)).on('change', bs.reload)
+})
 
-}
+gulp.task('watch', function() {
 
-async function clearDist() {
-	let deletedPath = await del(['dist'])
-}
+	watch('src/js/*.js', gulp.parallel('js'))
+	watch('src/sass/**/*', gulp.parallel('sass'))
+	watch('src/html/**/*', gulp.parallel('html'))
 
-function minImages() {
-	return gulp.src('src/img/*.*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'))
-}
+})
+
+gulp.task('svg', function() {
+
+	return gulp.src('./src/img/**/*.svg')
+		.pipe(cheerio({
+			run: function($) {
+				$('[fill]').removeAttr('fill')
+				$('[stroke]').removeAttr('stroke')
+				$('[style]').removeAttr('style')
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(replace('&gt;', '>'))
+		.pipe(sprite({
+			mode: {
+				symbol: {
+					sprite: "sprite.svg",
+				}
+			}
+		}))
+		.pipe(gulp.dest('build/img'))
+})
 
 
-function sassCompile() {
-	return gulp.src('src/sass/main.sass')
-		.pipe(sass())
-		.pipe(prefixer({
-			browsers: ['last 2 version'],
-			cascade: false
-			}))
-		.pipe(csso())
-		.pipe(gulp.dest('dist/css'))
-		.pipe(bs.stream())
-}
+gulp.task('html', function() {
 
-
-function compressJS() {
-	return pipeline(
-
-			gulp.src('src/js/*.js')
-				.pipe(babel({
-					presets: ['@babel/env']
-				})),
-			uglify(),
-			gulp.dest('dist/js')
-
-		)
-}
-
-function minifyHtml() {
-	return gulp.src('src/index.html')
+	return gulp.src('src/html/*.html')
+		.pipe(fileinc({
+			prefix: '@@'
+		}))
 		.pipe(htmlmin({
 			collapseWhitespace: true
-			}))
-		.pipe(gulp.dest('dist'))
-}
+		}))
+		.pipe(gulp.dest('build/'))
+		.pipe(bs.stream())
+
+})
+
+gulp.task('imagemin', function() {
+
+	gulp.src('src/img/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img'))
+
+})
 
 
-exports.build = gulp.series(sassCompile, minifyHtml, compressJS, minImages, streamMe)
-exports.streamMe = streamMe;
-exports.clearDist = clearDist;
-exports.minImages = minImages
+gulp.task('js', function() {
+
+	return gulp.src('src/js/*.js')
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('build/js'))
+		.pipe(bs.stream())
+
+})
+
+
+
+gulp.task('sass', function() {
+
+	return gulp.src('src/sass/main.sass')
+		.pipe(plumber({
+			errorHandler: notify.onError(function(err) {
+				return {
+					title: 'Sass styles',
+					sound: false,
+					message: err.message
+				}
+				})
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(prefixer({
+			overrideBrowserslist: ['last 4 versions']
+		}))
+		.pipe(gulp.dest('build/css'))
+		.pipe(bs.stream())
+
+})
+
+
+
+gulp.task('del', function(cb) {
+
+	del.sync('build/');
+	cb()
+
+})
+
+
+gulp.task('dev', gulp.parallel('del', 'sass', 'js', 'html', 'imagemin', 'svg', 'watch', 'server'))
